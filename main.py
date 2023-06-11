e953fdfb20fed21ad2ac9d278435e875eb2ddc68
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-11 09:00:02 -0400

    Update main.py
    add gui?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
 import numpy as np
+import tkinter as tk
 
 # Step 1: Gather and preprocess user data
 
@@ -19,6 +20,51 @@ data['is_male'] = (data['gender'] == 'male').astype(int)
 X = data[['age_in_days', 'is_male', 'product_interest']]
 y = data['purchase_history']
 
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Recommendation System")
+window.geometry("400x200")
+
+# Create a function to handle button click event
+def recommend_handler():
+    # Get user input from entry fields
+    age = int(age_entry.get())
+    gender = gender_var.get()
+    product_interest = product_entry.get()
+    
+    # Create user data dataframe
+    user_data = pd.DataFrame({'age_in_days': [age], 'is_male': [gender], 'product_interest': [product_interest]})
+    
+    # Generate recommendations
+    recommendations = recommend(user_data)
+    
+    # Display recommendations in the result label
+    result_label.configure(text="Recommendations:\n" + "\n".join(recommendations))
+    
+# Create GUI elements
+age_label = tk.Label(window, text="Age:")
+age_label.pack()
+age_entry = tk.Entry(window)
+age_entry.pack()
+
+gender_label = tk.Label(window, text="Gender:")
+gender_label.pack()
+gender_var = tk.StringVar()
+gender_radio_male = tk.Radiobutton(window, text="Male", variable=gender_var, value=1)
+gender_radio_male.pack()
+gender_radio_female = tk.Radiobutton(window, text="Female", variable=gender_var, value=0)
+gender_radio_female.pack()
+
+product_label = tk.Label(window, text="Product Interest:")
+product_label.pack()
+product_entry = tk.Entry(window)
+product_entry.pack()
+
+recommend_button = tk.Button(window, text="Recommend", command=recommend_handler)
+recommend_button.pack()
+
+result_label = tk.Label(window, text="Recommendations will appear here")
+result_label.pack()
 
 # Step 2: Implement machine learning algorithms
 
