ec0b0c7fa233e389673036ff3bf010e5136a8c81
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-11 09:05:51 -0400

    Update main.py
    simple

--- main.py
+++ main.py
@@ -1,46 +1,11 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
-import numpy as np
 import tkinter as tk
-
-# Step 1: Gather and preprocess user data
-
-# Collect data from various sources
-data = pd.read_csv('user_data.csv')
-
-# Clean the data
-data = data.dropna()
-data = data.drop_duplicates()
-
-# Perform feature engineering
-data['age_in_days'] = (pd.to_datetime('today') - pd.to_datetime(data['birth_date'])).dt.days
-data['is_male'] = (data['gender'] == 'male').astype(int)
-
-# Prepare data for analysis
-X = data[['age_in_days', 'is_male', 'product_interest']]
-y = data['purchase_history']
+from recommendation_system import recommend_handler
 
 # Create a Tkinter window
 window = tk.Tk()
 window.title("Recommendation System")
-window.geometry("400x200")
+window.geometry("400x250")
 
-# Create a function to handle button click event
-def recommend_handler():
-    # Get user input from entry fields
-    age = int(age_entry.get())
-    gender = gender_var.get()
-    product_interest = product_entry.get()
-    
-    # Create user data dataframe
-    user_data = pd.DataFrame({'age_in_days': [age], 'is_male': [gender], 'product_interest': [product_interest]})
-    
-    # Generate recommendations
-    recommendations = recommend(user_data)
-    
-    # Display recommendations in the result label
-    result_label.configure(text="Recommendations:\n" + "\n".join(recommendations))
-    
 # Create GUI elements
 age_label = tk.Label(window, text="Age:")
 age_label.pack()
@@ -66,76 +31,4 @@ recommend_button.pack()
 result_label = tk.Label(window, text="Recommendations will appear here")
 result_label.pack()
 
-# Step 2: Implement machine learning algorithms
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Create a collaborative filtering model
-cf_model = CollaborativeFiltering()
-cf_model.fit(X_train, y_train)
-
-# Create a content-based filtering model
-cb_model = ContentBasedFiltering()
-cb_model.fit(X_train, y_train)
-
-# Create a hybrid filtering model
-hybrid_model = HybridFiltering(cf_model, cb_model)
-hybrid_model.fit(X_train, y_train)
-
-
-# Step 3: Develop a recommendation system
-
-# Create a function that takes user data as input and generates recommendations
-def recommend(user_data):
-    # Use the hybrid filtering model to generate recommendations
-    recommendations = hybrid_model.predict(user_data)
-    
-    # Return the top 5 recommendations
-    return recommendations.head(5)
-
-
-# Step 4: Test and evaluate the system
-
-# Use cross-validation to evaluate the system's performance
-scores = cross_val_score(hybrid_model, X, y, cv=5)
-print(f"Mean cross-validation score: {np.mean(scores)}")
-
-# Use A/B testing to evaluate the system's effectiveness
-test_data = pd.read_csv('test_data.csv')
-group_a = test_data[test_data['group'] == 'A']
-group_b = test_data[test_data['group'] == 'B']
-
-group_a_recs = group_a.apply(recommend, axis=1)
-group_b_recs = group_b.apply(recommend, axis=1)
-
-group_a_ctr = group_a_recs.apply(lambda x: len(set(x) & set(group_a['purchase_history'])) / len(x))
-group_b_ctr = group_b_recs.apply(lambda x: len(set(x) & set(group_b['purchase_history'])) / len(x))
-
-print(f"Group A conversion rate: {np.mean(group_a_ctr)}")
-print(f"Group B conversion rate: {np.mean(group_b_ctr)}")
-
-
-# Step 5: Document the development process
-
-# Use Jupyter Notebook or Google Colab to document the code and analysis
-# Create clear and concise instructions for using the recommendation system
-# Include explanations of the data sources, preprocessing steps, machine learning algorithms, and evaluation metrics
-
-# Step 6: Showcase your project
-
-# Create a portfolio or GitHub repository to showcase the recommendation system
-# Include examples of how it can be used to provide value to users, such as increasing conversion rates or improving customer satisfaction
-# Highlight the key features and benefits of the recommendation system, such as its accuracy and personalization capabilities
-
-# Example usage of the recommendation system
-user_data = {'age_in_days': 10000, 'is_male': 1, 'product_interest': 'books'}
-recommendations = recommend(pd.DataFrame(user_data, index=[0]))
-print(recommendations)
-
-# Key features and benefits of the recommendation system
-# - Uses machine learning to generate personalized recommendations based on user data
-# - Incorporates collaborative filtering and content-based filtering for improved accuracy
-# - Supports hybrid filtering by combining the strengths of both approaches
-# - Evaluates system performance through cross-validation and A/B testing
-# - Can increase conversion rates and improve customer satisfaction through personalized recommendations
+window.mainloop()
