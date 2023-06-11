7f318903da3f8288f8f67e3db1d3d72db9bcf31f
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-11 09:06:14 -0400

    Update recommendation_system.py

--- recommendation_system.py
+++ recommendation_system.py
@@ -1,107 +1,32 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
-import numpy as np
-import tkinter as tk
+from collaborative_filtering import CollaborativeFiltering
+from content_based_filtering import ContentBasedFiltering
+from hybrid_filtering import HybridFiltering
 
-# Step 1: Gather and preprocess user data
-
-# Collect data from various sources
+# Gather and preprocess user data
 data = pd.read_csv('user_data.csv')
-
-# Clean the data
 data = data.dropna()
 data = data.drop_duplicates()
-
-# Perform feature engineering
 data['age_in_days'] = (pd.to_datetime('today') - pd.to_datetime(data['birth_date'])).dt.days
 data['is_male'] = (data['gender'] == 'male').astype(int)
-
-# Prepare data for analysis
 X = data[['age_in_days', 'is_male', 'product_interest']]
 y = data['purchase_history']
 
-# Create a Tkinter window
-window = tk.Tk()
-window.title("Recommendation System")
-window.geometry("400x250")
-
-# Create a function to handle button click event
-def recommend_handler():
-    # Get user input from entry fields
-    age = int(age_entry.get())
-    gender = gender_var.get()
-    product_interest = product_entry.get()
-    
-    # Create user data dataframe
-    user_data = pd.DataFrame({'age_in_days': [age], 'is_male': [gender], 'product_interest': [product_interest]})
-    
-    # Generate recommendations
-    recommendations = recommend(user_data)
-    
-    # Display recommendations in the result label
-    result_label.configure(text="Recommendations:\n" + "\n".join(recommendations))
-    
-# Create GUI elements
-age_label = tk.Label(window, text="Age:")
-age_label.pack()
-age_entry = tk.Entry(window)
-age_entry.pack()
-
-gender_label = tk.Label(window, text="Gender:")
-gender_label.pack()
-gender_var = tk.StringVar()
-gender_radio_male = tk.Radiobutton(window, text="Male", variable=gender_var, value=1)
-gender_radio_male.pack()
-gender_radio_female = tk.Radiobutton(window, text="Female", variable=gender_var, value=0)
-gender_radio_female.pack()
-
-product_label = tk.Label(window, text="Product Interest:")
-product_label.pack()
-product_entry = tk.Entry(window)
-product_entry.pack()
-
-recommend_button = tk.Button(window, text="Recommend", command=recommend_handler)
-recommend_button.pack()
-
-result_label = tk.Label(window, text="Recommendations will appear here")
-result_label.pack()
-
-# Step 2: Implement machine learning algorithms
-
-# Split the data into training and testing sets
+# Implement machine learning algorithms
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Create a collaborative filtering model
 cf_model = CollaborativeFiltering()
 cf_model.fit(X_train, y_train)
-
-# Create a content-based filtering model
 cb_model = ContentBasedFiltering()
 cb_model.fit(X_train, y_train)
-
-# Create a hybrid filtering model
 hybrid_model = HybridFiltering(cf_model, cb_model)
 hybrid_model.fit(X_train, y_train)
 
-
-# Step 3: Develop a recommendation system
-
-# Create a function that takes user data as input and generates recommendations
+# Develop a recommendation system
 def recommend(user_data):
-    # Use the hybrid filtering model to generate recommendations
     recommendations = hybrid_model.predict(user_data)
-    
-    # Return the top 5 recommendations
     return recommendations.head(5)
 
-
-# Step 4: Test and evaluate the system
-
-# Use cross-validation to evaluate the system's performance
+# Test and evaluate the system
 scores = cross_val_score(hybrid_model, X, y, cv=5)
 print(f"Mean cross-validation score: {np.mean(scores)}")
-
-# Use A/B testing to evaluate the system's effectiveness
-test_data = pd.read_csv('test_data.csv')
-group_a = test_data[test_data['group'] == 'A']
-group_b = test_data[test_data['group'] == 'B']
