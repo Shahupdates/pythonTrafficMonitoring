485ffb4351e023db5c15397b19ae4f0c5acccc3d
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-16 17:19:50 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,36 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Document the development process
+def main():
+    # Document the development process
 
-# Example usage of the recommendation system
-user_data = {'age_in_days': 10000, 'is_male': 1, 'product_interest': 'books'}
-recommendations = recommend(pd.DataFrame(user_data, index=[0]))
-print(recommendations)
+    # Example usage of the recommendation system
+    user_data = {'age_in_days': 10000, 'is_male': 1, 'product_interest': 'books'}
+    recommendations = recommend(pd.DataFrame(user_data, index=[0]))
+    print(recommendations)
 
-# Key features and benefits of the recommendation system
+    # Key features and benefits of the recommendation system
 
-# Visualization examples
-data = pd.read_csv('user_data.csv')
-plt.hist(data['age_in_days'], bins=20)
-plt.xlabel('Age (in days)')
-plt.ylabel('Frequency')
-plt.title('User Age Distribution')
-plt.show()
+    # Visualization examples
+    data = pd.read_csv('user_data.csv')
+    plt.hist(data['age_in_days'], bins=20)
+    plt.xlabel('Age (in days)')
+    plt.ylabel('Frequency')
+    plt.title('User Age Distribution')
+    plt.show()
 
-# Example visualization of model performance
-plt.boxplot(scores)
-plt.xlabel('Model')
-plt.ylabel('Cross-validation score')
-plt.title('Model Performance')
-plt.xticks([1], ['Hybrid Filtering'])
-plt.show()
+    # Example visualization of model performance
+    plt.boxplot(scores)
+    plt.xlabel('Model')
+    plt.ylabel('Cross-validation score')
+    plt.title('Model Performance')
+    plt.xticks([1], ['Hybrid Filtering'])
+    plt.show()
 
-# Example visualization of product interest preferences
-product_counts = data['product_interest'].value_counts()
-plt.bar(product_counts.index, product_counts.values)
-plt.xlabel('Product Interest')
-plt.ylabel('Count')
-plt.title('Product Interest Preferences')
-plt.xticks(rotation=45)
-plt.show()
+    # Example visualization of product interest preferences
+    product_counts = data['product_interest'].value_counts()
+    plt.bar(product_counts.index, product_counts.values)
+    plt.xlabel('Product Interest')
+    plt.ylabel('Count')
+    plt.title('Product Interest Preferences')
+    plt.xticks(rotation=45)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
